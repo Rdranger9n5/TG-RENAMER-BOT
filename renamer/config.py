0f938044556f8cb33cc711fda408d1fb0e2a7a5a
Rdranger9n5 <145197281+Rdranger9n5@users.noreply.github.com>
2024-04-15 23:51:00 -0700

    Update config.py

--- renamer/config.py
+++ renamer/config.py
@@ -15,7 +15,7 @@ class Config:
     BOT_PASSWORD = os.environ.get("BOT_PASSWORD", "") if os.environ.get("BOT_PASSWORD", "") else None
     CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION") if os.environ.get("CUSTOM_CAPTION", "") else None
     FORCE_SUB = os.environ.get("FORCE_SUB", "") if os.environ.get("FORCE_SUB", "") else None
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Rolexfilter:Rolexfilter@rolex.0o0saa0.mongodb.net/?retryWrites=true&w=majority")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     try:
         TIME_GAP = int(os.environ.get("TIME_GAP", "")) if os.environ.get("TIME_GAP", "") else None
